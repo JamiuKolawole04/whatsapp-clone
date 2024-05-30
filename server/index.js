6944fa56960089ee9a4470a9b0823e07041c2ff7
JamiuKolawole04 <Jakko4real@gmail.com>
2024-05-30 12:17:45 +0100

    chore: server ste up

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const server = app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
