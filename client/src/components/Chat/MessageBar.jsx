3e25b2a6b4a0f5b8cc0a28b7e1ee4e78ef6e0f8c
JamiuKolawole04 <Jakko4real@gmail.com>
2024-07-16 00:01:45 +0100

    feat: emoji picker done

--- client/src/components/Chat/MessageBar.jsx
+++ client/src/components/Chat/MessageBar.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { BsEmojiSmile } from "react-icons/bs";
 import { FaMicrophone } from "react-icons/fa";
 import { ImAttachment } from "react-icons/im";
 import { MdSend } from "react-icons/md";
+import EmojiPicker from "emoji-picker-react";
 
 import { useStateProvider } from "@/context/StateContext";
 import { ADD_MESSAGE_ROUTE } from "@/utils/ApiRoutes";
@@ -13,6 +14,35 @@ function MessageBar() {
   const [{ userInfo, currentChatUser, socket }, dispatch] = useStateProvider();
 
   const [message, setMessage] = useState("");
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const emojiPickerRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (event.target.id !== "emoji-open") {
+        if (
+          emojiPickerRef.current &&
+          !emojiPickerRef.current.contains(event.target)
+        ) {
+          setShowEmojiPicker(false);
+        }
+      }
+    };
+
+    document.addEventListener("click", handleOutsideClick);
+
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
+
+  const handleEmojiModal = () => {
+    setShowEmojiPicker((prev) => !prev);
+  };
+
+  const handleEmojiClick = (emoji) => {
+    setMessage((prevMessage) => (prevMessage += emoji.emoji));
+  };
 
   const sendMessage = async () => {
     try {
@@ -46,7 +76,19 @@ function MessageBar() {
         <BsEmojiSmile
           className="text-panel-header-icon cursor-pointer text-xl"
           title="Emoji"
+          id="emoji-open"
+          onClick={handleEmojiModal}
         />
+
+        {showEmojiPicker && (
+          <div
+            className="absolute bottom-24 left-16 z-40
+          "
+            ref={emojiPickerRef}
+          >
+            <EmojiPicker onEmojiClick={handleEmojiClick} theme="dark" />
+          </div>
+        )}
 
         <ImAttachment
           className="text-panel-header-icon cursor-pointer text-xl"
