228851d3e935865dd38281ac65f1b02c0fcf55e7
JamiuKolawole04 <Jakko4real@gmail.com>
2024-07-19 23:58:08 +0100

    chore: development ongoing

--- client/src/components/common/CaptureAudio.jsx
+++ client/src/components/common/CaptureAudio.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaTrash } from "react-icons/fa";
 
 function CaptureAudio({ hide }) {
+  const [isRecording, setIsRecording] = useState(false);
+  const [recordedAudio, setRecordedAudio] = useState(null);
+  const [waveform, setWaveform] = useState(null);
+  const [recordingDuration, setRecordingDuration] = useState(0);
+  const [currentPlaybackTime, setCurrentPlaybackTime] = useState(0);
+  const [totalDuration, setTotalDuration] = useState(0);
   return (
     <div className="flex text-2xl w-full justify-end items-center">
       <div className="pt-1">
         <FaTrash className="text-panel-header-icon" onClick={() => hide()} />
       </div>
 
-      <div className="mx-4 px-4 text-white text-lg flex gap-3 justify-center items-start"></div>
+      <div className="mx-4 px-4 text-white text-lg flex gap-3 justify-center items-center bg-search-input-container-background rounded-full drop-shadow-lg">
+        {isRecording ? (
+          <div className="text-red-500 animate-pulse 2-60 text-center">
+            Recording
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
     </div>
   );
 }
 
 export default CaptureAudio;
-git;
