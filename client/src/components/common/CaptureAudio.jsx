d864b9932fd543b5823cb84c84af242e2d3b7855
JamiuKolawole04 <Jakko4real@gmail.com>
2024-07-20 01:02:51 +0100

    chore: adding format time function

--- client/src/components/common/CaptureAudio.jsx
+++ client/src/components/common/CaptureAudio.jsx
@@ -1,27 +1,133 @@
-import React, { useState } from "react";
-import { FaTrash } from "react-icons/fa";
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import {
+  FaMicrophone,
+  FaPauseCircle,
+  FaPlay,
+  FaStop,
+  FaTrash,
+} from "react-icons/fa";
+import WaveSurfer from "wavesurfer.js";
+import { MdSend } from "react-icons/md";
+
+import { useStateProvider } from "@/context/StateContext";
 
 function CaptureAudio({ hide }) {
+  const [{ userInfo, currentChatUser, socket }, dispatch] = useStateProvider();
+
   const [isRecording, setIsRecording] = useState(false);
   const [recordedAudio, setRecordedAudio] = useState(null);
   const [waveform, setWaveform] = useState(null);
   const [recordingDuration, setRecordingDuration] = useState(0);
   const [currentPlaybackTime, setCurrentPlaybackTime] = useState(0);
   const [totalDuration, setTotalDuration] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const audioRef = useRef(null);
+  const mediaRecorderRef = useRef(null);
+  const waveformRef = useRef(null);
+
+  useEffect(() => {
+    const wavesurfer = WaveSurfer.create({
+      container: waveformRef.current,
+      waveColor: "#ccc",
+      progressColor: "#4a9eff",
+      cursorColor: "#7ae3c3",
+      barWidth: 2,
+      height: 30,
+      responsive: true,
+    });
+
+    setWaveform(wavesurfer);
+    wavesurfer.on("finish", () => {
+      setIsPlaying(false);
+    });
+
+    return wavesurfer.destroy();
+  }, []);
+
+  useEffect(() => {
+    if (waveform) {
+      handleStartRecording();
+    }
+  }, [waveform]);
+
+  const hanldePlayRecording = () => {};
+
+  const handlePauseRecording = () => {};
+
+  const handleStartRecording = () => {};
+
+  const handleStopRecording = () => {};
+
+  const sendRecording = async () => {};
+
+  const formatTime = (time) => {
+    if (isNaN(time)) {
+      return "00:00";
+    }
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
   return (
     <div className="flex text-2xl w-full justify-end items-center">
       <div className="pt-1">
         <FaTrash className="text-panel-header-icon" onClick={() => hide()} />
       </div>
 
-      <div className="mx-4 px-4 text-white text-lg flex gap-3 justify-center items-center bg-search-input-container-background rounded-full drop-shadow-lg">
+      <div className="mx-4 py-3 text-white text-lg flex gap-3 justify-center items-center bg-search-input-container-background rounded-full drop-shadow-lg">
         {isRecording ? (
           <div className="text-red-500 animate-pulse 2-60 text-center">
-            Recording
+            Recording <span>{recordingDuration}s</span>
           </div>
         ) : (
-          <div></div>
+          <div>
+            {recordedAudio && (
+              <Fragment>
+                {!isPlaying ? (
+                  <FaPlay onClick={hanldePlayRecording} />
+                ) : (
+                  <FaStop onClick={handlePauseRecording} />
+                )}
+              </Fragment>
+            )}
+          </div>
         )}
+
+        <div className="w-60" ref={waveformRef} hidden={isRecording} />
+        {recordedAudio && isPlaying && (
+          <span>{formatTime(currentPlaybackTime)}</span>
+        )}
+        {recordedAudio && !isPlaying && (
+          <span>{formatTime(totalDuration)}</span>
+        )}
+
+        <audio ref={audioRef} hidden />
+
+        <div className="mr-4">
+          {!isRecording ? (
+            <FaMicrophone
+              className="text-red-500"
+              onClick={handleStartRecording}
+            />
+          ) : (
+            <FaPauseCircle
+              className="text-red-500"
+              onClick={handleStopRecording}
+            />
+          )}
+        </div>
+
+        <div>
+          <MdSend
+            className="text-panel-header-icon cursor-pointer mr-4"
+            title="Send"
+            onClick={sendRecording}
+          />
+        </div>
       </div>
     </div>
   );
